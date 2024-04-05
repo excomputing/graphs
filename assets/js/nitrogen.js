64da4dddb558b076da6badda72f5d8db76a0a204
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-04-05 20:03:16 +0100

    Update assets/js/nitrogen.js

---
+++ assets/js/nitrogen.js
@@ -0,0 +1,313 @@
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+var url = 'https://raw.githubusercontent.com/membranes/graphs/develop/assets/menu/nitrogen.json';
+
+
+$.getJSON(url, function (data) {
+
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.desc).text(entry.name));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption);
+
+});
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected);
+});
+
+
+// Generate graphs
+function generateChart(fileNameKey) {
+
+    $.getJSON('https://raw.githubusercontent.com/membranes/graphs/develop/sample/nitrogen/pollutant_8_station_' + fileNameKey
+     + '.json', function (source) {
+
+        // https://api.highcharts.com/highstock/plotOptions.series.dataLabels
+        // https://api.highcharts.com/class-reference/Highcharts.Point#.name
+        // https://api.highcharts.com/highstock/tooltip.pointFormat
+
+
+        // split the data set into ohlc and medians
+        var ohlc = [],
+            medians = [],
+            maxima = [],
+            minima = [],
+            numbers = [],
+            cumulative = [],
+            dataLength = source.data.length,
+            groupingUnits = [[
+                'day',   // unit name
+                [1]      // allowed multiples
+            ]],
+            i = 0;
+
+        for (i; i < dataLength; i += 1) {
+
+            ohlc.push([
+                source.data[i][0], // the date
+                source.data[i][2], // lower quartile   open
+                source.data[i][5], // upper whisker  high
+                source.data[i][1], // lower whisker  low
+                source.data[i][4] // upper quartile  close
+            ]);
+
+            medians.push({
+                x: source.data[i][0], // the date
+                y: source.data[i][3] // median
+            });
+
+            maxima.push({
+                x: source.data[i][0], // the date
+                y: source.data[i][7] // maximum
+            });
+
+            minima.push({
+                x: source.data[i][0], // the date
+                y: source.data[i][6] // minimum
+            });
+
+            /*numbers.push({
+                x: source.data[i][0], // date
+                y: source.data[i][.]  // counts
+            });*/
+
+        }
+
+        Highcharts.setOptions({
+            lang: {
+                thousandsSep: ','
+            }
+        });
+
+        // Draw a graph
+        Highcharts.stockChart('container0003', {
+
+            rangeSelector: {
+                selected: 1,
+                verticalAlign: 'top',
+                floating: false,
+                inputPosition: {
+                    x: 0,
+                    y: 0
+                },
+                buttonPosition: {
+                    x: 0,
+                    y: 0
+                },
+                inputEnabled: true,
+                inputDateFormat: '%Y-%m-%d'
+            },
+
+            chart: {
+                zoomType: 'x'
+                // borderWidth: 2,
+                // marginRight: 100
+            },
+
+            title: {
+                text: 'Distributions of: ' + optionSelected
+            },
+
+            subtitle: {
+                text: '<p>Details</p> <br/> ' +
+                    '<p><b>Upcoming</b></p>'
+            },
+
+            time: {
+                // timezone: 'Europe/London'
+            },
+
+            credits: {
+                enabled: false
+            },
+
+            legend: {
+                enabled: true,
+                width: 600,
+                x: 100
+                // align: 'middle',
+                // layout: 'vertical',
+                // verticalAlign: 'bottom',
+                // y: 10,
+                // x: 35
+            },
+
+            caption: {
+                // verticalAlign: "top",
+                text: '<p>Herein, each candlestick illustrates the spread of nitrogen measures ' +
+                    'across 24 hours.</p>'
+            },
+
+            exporting: {
+                buttons: {
+                    contextButton: {
+                        menuItems: [ 'viewFullscreen', 'printChart', 'separator',
+                            'downloadPNG', 'downloadJPEG', 'downloadPDF', 'downloadSVG' , 'separator',
+                            'downloadXLS', 'downloadCSV']
+                    }
+                }
+            },
+
+            yAxis: [{
+                labels: {
+                    align: 'left',
+                    x: 9
+                },
+                title: {
+                    text: optionSelected,
+                    x: 0
+                },
+                min: 0,
+                height: '65%',
+                lineWidth: 2,
+                resize: {
+                    enabled: true
+                }
+            }, {
+                labels: {
+                    align: 'left',
+                    x: 9
+                },
+                title: {
+                    text: 'Minima',
+                    x: 0
+                },
+                top: '67.5%',
+                height: '31%',
+                offset: 0,
+                lineWidth: 2
+            }
+            ],
+
+            plotOptions: {
+                series: {
+                    turboThreshold: 4000
+                }
+            },
+
+            tooltip: {
+                split: true,
+                dateTimeLabelFormats: {
+                    millisecond: "%A, %e %b, %H:%M:%S.%L",
+                    second: "%A, %e %b, %H:%M:%S",
+                    minute: "%A, %e %b, %H:%M",
+                    hour: "%A, %e %b, %H:%M",
+                    day: "%A, %e %B, %Y",
+                    week: "%A, %e %b, %Y",
+                    month: "%B %Y",
+                    year: "%Y"
+                }
+
+            },
+
+            series: [{
+                type: 'candlestick',
+                name: 'Distribution of ' + optionSelected,
+                data: ohlc,
+                dataGrouping: {
+                    units: groupingUnits,
+                    dateTimeLabelFormats: {
+                        millisecond: ['%A, %e %b, %H:%M:%S.%L', '%A, %b %e, %H:%M:%S.%L', '-%H:%M:%S.%L'],
+                        second: ['%A, %e %b, %H:%M:%S', '%A, %b %e, %H:%M:%S', '-%H:%M:%S'],
+                        minute: ['%A, %e %b, %H:%M', '%A, %b %e, %H:%M', '-%H:%M'],
+                        hour: ['%A, %e %b, %H:%M', '%A, %b %e, %H:%M', '-%H:%M'],
+                        day: ['%A, %e %b, %Y', '%A, %b %e', '-%A, %b %e, %Y'],
+                        week: ['Week from %A, %e %b, %Y', '%A, %b %e', '-%A, %b %e, %Y'],
+                        month: ['%B %Y', '%B', '-%B %Y'],
+                        year: ['%Y', '%Y', '-%Y']
+                    }
+                },
+                tooltip: {
+                    pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {series.name} </b><br/>' +
+                        'Upper Whisker: {point.high:,.2f}<br/>' +
+                        'Upper Quartile: {point.close:,.2f}<br/>' +
+                        'Lower Quartile: {point.open:,.2f}<br/>' +
+                        'Lower Whisker: {point.low:,.2f}' + '<br/>'
+                }
+            },
+                {
+                    type: 'spline',
+                    name: 'Median',
+                    data: medians,
+                    color: '#6B8E23',
+                    yAxis: 0,
+                    dataGrouping: {
+                        units: groupingUnits
+                    },
+                    tooltip: {
+                        pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {series.name} </b>: ' +
+                            '{point.y:,.2f}<br/>'
+                    }
+                },
+                {
+                    type: 'spline',
+                    name: 'Maxima',
+                    data: maxima,
+                    color: '#A08E23',
+                    visible: false,
+                    yAxis: 0,
+                    dataGrouping: {
+                        units: groupingUnits
+                    },
+                    tooltip: {
+                        pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {series.name} </b>: ' +
+                            '{point.y}<br/>'
+                    }
+                },
+                {
+                    type: 'spline',
+                    name: 'Minima',
+                    data: minima,
+                    color: '#800000',
+                    yAxis: 1,
+                    dataGrouping: {
+                        units: groupingUnits
+                    },
+                    tooltip: {
+                        pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {series.name} </b>: ' +
+                            '{point.y}<br/>'
+                    }
+
+                }
+
+
+            ],
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 700
+                    },
+                    chartOptions: {
+                        rangeSelector: {
+                            inputEnabled: false
+                        }
+                    }
+                }]
+            }
+        });
+
+    }).fail(function () {
+        console.log("Missing");
+        $('#container0003').empty();
+    });
+
+}
+
